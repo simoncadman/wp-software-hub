efb1178bbbcfa7606f6218f5e4dab72830e05b5e
Simon Cadman <src@niftiestsoftware.com>
2012-12-18 21:32:31 +0000

    Added initial plugin code

---
+++ admin-options.php
@@ -0,0 +1,37 @@
+<div class="wrap">
+	<h2><?php _e('Software Hub Options', 'software_hub_control');?></h2>
+	<form method="post" action="options.php">
+		<?php settings_fields('software_hub_settings'); ?>
+		<h3><?php _e('Software', 'software_hub');?></h3>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><?php _e('Software', 'software_hub');?></th>
+                                <td>
+                                        <textarea name="software_hub_instances"><?php echo get_option('software_hub_instances', ""); ?></textarea>
+				</td>
+				<td>
+					<small><?php _e("List of software instances", 'software_hub');?></small>
+				</td>
+			</tr>
+		</table>
+                <?php foreach ( software_hub_get_software_instances() as $software ) : ?>
+                    <h3><?php echo $software; ?></h3>
+                    <h4>Overview</h4>
+                    <table class="form-table">
+                            <tr valign="top">
+                                    <th scope="row"><?php _e('Show Overview', 'software_hub');?></th>
+                                    <td>
+                                            <textarea name="software_hub_instances"><?php echo get_option('software_hub_instances', ""); ?></textarea>
+                                    </td>
+                                    <td>
+                                            <small><?php _e("List of software instances", 'software_hub');?></small>
+                                    </td>
+                            </tr>
+                    </table>
+                <?php endforeach; ?>
+                
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+	</form>
+</div>
---
+++ software-hub.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package Software-Hub
+ * @version 0.1
+ */
+/*
+Plugin Name: Software Hub
+Plugin URI: http://sh.niftiestsoftware.com
+Description: Manages software projects
+Author: Simon Cadman
+Version: 0.1
+Author URI: http://www.niftiestsoftware.com/
+*/
+/*  Copyright 2012 Simon Cadman (src@niftiestsoftware.com)
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function software_hub_get_software_instances ( ) {
+    $instances = get_option('software_hub_instances', "");
+    return explode("\n", $instances);
+}
+
+function software_hub_settings () {
+    register_setting( 'software_hub_settings', 'software_hub_instances');
+}
+
+function software_hub_add_pages() {
+    add_submenu_page( 'options-general.php', 'Software Hub', 'Software Hub', 'manage_options', 'software_hub_menu', 'software_hub_options' );
+}
+
+function software_hub_options () {
+    require_once(dirname(__FILE__) . '/admin-options.php');
+}
+
+add_action('admin_menu', 'software_hub_settings');
+add_action('admin_menu', 'software_hub_add_pages');
