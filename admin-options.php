f8e8300d8bef58722df8180ad4b70f10ae4b26d0
Simon Cadman <src@niftiestsoftware.com>
2012-12-22 23:15:20 +0000

    Show short commit id in backend

--- admin-options.php
+++ admin-options.php
@@ -106,7 +106,7 @@
                             <td>
                                 <ul>
                                     <?php foreach ( software_hub_changes($release->id) as $change ) : ?>
-                                    <li><?php echo $change->commit; ?> - <?php echo $change->note; ?></li>
+                                    <li><?php echo substr($change->commit, 0, 10); ?> - <?php echo $change->note; ?></li>
                                     <?php endforeach; ?>
                                 </ul>
                             </td>
