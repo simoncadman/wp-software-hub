b11bc3d169f890c6048e81e4bdf0fd4728854c71
Simon Cadman <src@niftiestsoftware.com>
2012-12-29 20:42:24 +0000

    Fixed text align in js, and js error when installation disabled

--- js/software-hub.js
+++ js/software-hub.js
@@ -1,24 +1,31 @@
 jQuery('.software-hub-view').tabs()
 var installdropdown = document.getElementById('installdropdown');
-installdropdown.style.display = "";
-softwareHubHideItems();
+if ( typeof installdropdown != 'undefined'  ) {
+	installdropdown.style.display = "";
+	softwareHubHideItems();
+}
+
 function softwareHubHideItems ( ) {
-var items = document.getElementsByClassName('installtypes');
-for ( var i=0;i<items.length; i++ ) {
-items[i].style.display = "none";
+	var items = document.getElementsByClassName('installtypes');
+	for ( var i=0;i<items.length; i++ ) {
+		items[i].style.display = "none";
+	}
 }
-}
+
 function softwareHubUpdateInstallInstructions ( type ) {
-if ( typeof type == "undefined" ) {
-type=document.getElementById('installdropdown').value;
+
+	if ( typeof type == "undefined" ) {
+		type=document.getElementById('installdropdown').value;
+	}
+
+	softwareHubHideItems();
+	var installdescription = document.getElementById('install' + type );
+	if ( installdescription != null ) {
+		installdescription.style.display = "";
+	}
 }
-softwareHubHideItems();
-var installdescription = document.getElementById('install' + type );
-if ( installdescription != null ) {
-installdescription.style.display = "";
-}
-}
+
 document.body.onload = function () {
-softwareHubUpdateInstallInstructions();
+	softwareHubUpdateInstallInstructions();
 }
 
