e42e977205272cce3bf51740fb5cf3a663456e48
Simon Cadman <src@niftiestsoftware.com>
2013-01-01 01:04:28 +0000

    Only show live software on frontend

--- software-hub.php
+++ software-hub.php
@@ -330,7 +330,7 @@ where parent_id = %s or {$wpdb->prefix}software_hub_os_group.id = %s
             
             $installitems = $wpdb->get_results($wpdb->prepare("SELECT * from {$wpdb->prefix}software_hub_install where software_id = %s ", $params['id'] ));
             
-            $releases = $wpdb->get_results($wpdb->prepare("SELECT * from {$wpdb->prefix}software_hub_software_release where software_id = %s order by time desc ", $params['id'] ));
+            $releases = $wpdb->get_results($wpdb->prepare("SELECT * from {$wpdb->prefix}software_hub_software_release where software_id = %s and live = 1 order by time desc ", $params['id'] ));
             
             foreach ( $releases as $release ) {
                 $changes = $wpdb->get_results($wpdb->prepare("SELECT * from {$wpdb->prefix}software_hub_changelog where software_release_id = %s and live = 1 order by display_message desc ", $release->id ));
