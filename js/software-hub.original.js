6846984dbbe918d7360f5781a7e486632a7da25e
Niftyec2 root <src@niftiestsoftware.com>
2013-12-02 23:23:09 +0000

    Compressed js and css

---
+++ js/software-hub.original.js
@@ -0,0 +1,34 @@
+
+var installdropdown = document.getElementById('installdropdown');
+if ( installdropdown != null  ) {
+	installdropdown.style.display = "";
+	softwareHubHideItems();
+}
+
+function softwareHubHideItems ( ) {
+	var items = document.getElementsByClassName('installtypes');
+	for ( var i=0;i<items.length; i++ ) {
+		items[i].style.display = "none";
+	}
+}
+
+function softwareHubUpdateInstallInstructions ( type ) {
+
+        if ( document.getElementById('installdropdown') != null ) {
+            if ( typeof type == "undefined" ) {
+                    type=document.getElementById('installdropdown').value;
+            }
+
+            softwareHubHideItems();
+            var installdescription = document.getElementById('install' + type );
+            if ( installdescription != null ) {
+                    installdescription.style.display = "";
+            }
+        }
+}
+
+$(document).ready(function() {
+        $('.software-hub-view').tabs();
+	softwareHubUpdateInstallInstructions();
+});
+
